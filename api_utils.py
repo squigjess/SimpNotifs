9c7ac0f17892d0f550f2b3ca28990495b99337ae
Jessica Merriman <jpmerriman95@gmail.com>
2021-09-15 12:44:24 +1000

    Header checks now use variables for header names
    Changed main program flow to use less `elif`s

--- api_utils.py
+++ api_utils.py
@@ -2,23 +2,23 @@
 import hmac
 import hashlib
 
-expectedHeaders = [
-    "Twitch-Eventsub-Message-Id",
-    "Twitch-Eventsub-Message-Timestamp",
-    "Twitch-Eventsub-Message-Signature",
-    "Twitch-Eventsub-Subscription-Type",
-    "Twitch-Eventsub-Message-Type"
-]
+headerType = {
+    "msgID"        : "Twitch-Eventsub-Message-Id",
+    "msgTimestamp" : "Twitch-Eventsub-Message-Timestamp",
+    "msgSignature" : "Twitch-Eventsub-Message-Signature",
+    "subType"      : "Twitch-Eventsub-Subscription-Type",
+    "msgType"      : "Twitch-Eventsub-Message-Type"
+}
 
-def verifyChallenge(secret, digest, sentsignature):
+def verifyChallenge(secret, digest, sentSignature):
     """Takes a digest, encodes it with HMAC SHA256, then compares it to sentSignature for verification"""
     calculatedSignature = hmac.new(secret.encode("utf-8"), digest.encode("utf-8"), hashlib.sha256).hexdigest()
     calculatedSignatureCheck = "sha256={}".format(calculatedSignature)
-    return calculatedSignatureCheck == sentsignature
+    return calculatedSignatureCheck == sentSignature
 
 def containsExpectedHeaders(requestHeaders):
     """Checks the incoming request's headers to see if the expected headers are present"""
-    for header in expectedHeaders:
+    for header in headerType:
         if header not in requestHeaders:
 			print("Header missing: {}".format(header)) # For grep-ing logs
             return False
--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,9 @@
-"""Defines the API endpoints, handles incoming event triggers, sends outgoing
-webhook triggers.
-"""
+"""Defines the API endpoints, handles incoming event triggers, sends outgoing webhook triggers."""
 
 from flask import Flask, request, Response
 import requests
 import json
-import api_utils
+from api_utils import containsExpectedHeaders, verifyChallenge, headers
 import secrets # A file declaring strings that match up to secrets, keys, and IDs.
 
 # Accepts "\@here", "\@everyone", or a role ID as a string (looks like "<@&00000000>", which you can get by mentioning \@ROLENAME in your server).
@@ -23,31 +21,27 @@ def handleCallback():
 
     # First off, check if the necessary headers are present.
     # TODO: find a clean, neat way to have containsExpectedHeaders() return [bool, missingHeader]
-    if not api_utils.containsExpectedHeaders(request.headers):
+    if not containsExpectedHeaders(request.headers):
         return Response("Malformed request, missing expected headers.", status=403)
 
     # If all necessary headers are present, we can start checking if the signature given in the callback response is valid.
     # https://dev.twitch.tv/docs/eventsub#verify-a-signature
-    doesSignatureMatch = api_utils.verifyChallenge( secrets.EVENTSUB_SECRET,
-                                                    request.headers["Twitch-Eventsub-Message-Id"]+request.headers["Twitch-Eventsub-Message-Timestamp"]+request.data.decode("utf-8"),
-                                                    request.headers["Twitch-Eventsub-Message-Signature"])
+    doesSignatureMatch = verifyChallenge(secrets.EVENTSUB_SECRET,
+                                         request.headers[headerType["msgID"]]+request.headers[headerType["msgTimestamp"]]+request.data.decode("utf-8"),
+                                         request.headers[headerType["msgSignature"]])
     # Begin processing the callback request...
     if not doesSignatureMatch:
         return Response("Signature did not match.", status=403)
 
-    elif request.headers["Twitch-Eventsub-Subscription-Type"] != "stream.online":
+    if request.headers[headerType["subType"]] != "stream.online":
         return Response("Invalid request type.", status=403)
 
     # Is this a callback for registering a new stream.online event subscription?
-    elif request.headers["Twitch-Eventsub-Message-Type"] == "webhook_callback_verification":
+    if request.headers[headerType["msgType"]] == "webhook_callback_verification":
         return Response(requestData["challenge"], status=200)
 
     # Is the response an actual stream.online event?
-    elif request.headers["Twitch-Eventsub-Message-Type"] == "notification":
-        # These will be stored in the log file for debugging/troubleshooting purposes.
-        print(request.headers)
-        print(json.dumps(requestData,indent=4))
-
+    if request.headers[headerType["msgType"]] == "notification":
         # Paramters for the webhook "bot".
         streamerName = requestData["event"]["broadcaster_user_name"]
         streamURL    = "https://twitch.tv/{}".format(streamerName)
